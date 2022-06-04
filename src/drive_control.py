126b02f3843396ec6a3a54eea58aa94ef6584efc
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2022-06-04 12:47:51 -0700

    Update drive_control.py

--- src/drive_control.py
+++ src/drive_control.py
@@ -23,7 +23,7 @@ from math import copysign
 from dynamic_reconfigure.server import Server
 from copilot_interface.cfg import copilotControlParamsConfig
 from rov_control_interface.msg import rov_sensitivity
-from rov_control_interface.msg import PID
+import PID
 
 rospy.init_node("drive_control")
 linearJoyAxisFBIndex = 1  # forward-backward axis index in the joy topic array from the logitech Extreme 3D Pro
