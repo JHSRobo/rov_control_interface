ea8c4a23036c9af45e91f0a20c0f895fd8f9c7e5
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2022-11-11 12:06:32 -0800

    initial DH testing
    seeing if it receives data and is able to publish it, too

--- src/autonomous.py
+++ src/autonomous.py
@@ -1,1 +1,93 @@
+#!/usr/bin/env python
+# author Alex Bertran | v0.0 | 11-11-2022
+# Current imports are all those from drive_control.py and some may not be necessary
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
 
+from std_msgs.msg import UInt8  # For camera  pub
+from std_msgs.msg import Bool  # For TCU relay and solenoid controller pub and for pids
+from std_msgs.msg import Float64  # For pids
+from std_msgs.msg import Float32
+from nav_msgs.msg import Odometry
+
+from math import copysign
+from dynamic_reconfigure.server import Server
+from copilot_interface.cfg import copilotControlParamsConfig
+from rov_control_interface.msg import rov_sensitivity
+
+rospy.init_node("autonomous_control")
+
+thrustEN = False
+dhEnable = False
+
+def controlCallback(config, level):
+  global thrustEN, dhEnable, p_scalar, i_scalar, d_scalar
+  
+  thrustEN = config.thrusters
+  p_scalar = config.p_scalar
+  i_scalar = config.i_scalar
+  d_scalar = config.d_scalar
+  dhEnable = config.dh_enable
+  
+  return config
+
+def ROS_INFO_STREAM(thrustEN):
+  pass
+
+def thrusterStatusCallback(data):
+  global thrustEN
+  thrustEN = data.data
+  ROS_INFO_STREAM(thrustEN)
+
+def depthHoldCallback(data):
+  global p_scalar, i_scalar, d_scalar
+    
+  p_scalar = data.p_scalar
+  i_scalar = data.i_scalar
+  d_scalar = data.d_scalar
+
+def dhToggleCallback(data):
+  global dhEnable
+  
+  dhEnable = data.data
+  
+def dhStateCallback(data):
+  global dhMostRecentDepth
+  
+  if dhEnable: # only update depth if depth hold is disabled {dhEnable == False}
+    dhMostRecentDepth = data.pose.pose.position.z * -1
+    depth = Float64()
+    depth.data = dhMostRecentDepth
+    dh_setpoint_pub.publish(depth)
+  
+def dhControlEffortCallback(data): # no need for dhEnable check since PIDs won't publish control effort when disabled
+  global dh_eff
+  
+  dh_eff = data.data
+  
+def change_depth_callback(data, depth):
+  global dhEnable, thrustEN
+  
+  if thrustEN and dhEnable:
+    currentDepth = abs((depth.data - 198.3) / (893.04 / 149))
+    test_pub.publish(currentDepth)
+  
+def main():
+  global thruster_status_sub, depth_hold_sub, dh_state_sub, dh_ctrl_eff_sub, dh_toggle_sub, depth_sub
+  
+  thruster_status_sub = rospy.Subscriber('rov/thruster_status', Bool,thrusterStatusCallback)
+  depth_hold_sub = rospy.Subscriber('depth_hold/pid_enable', PID, depthHoldCallback)
+  dh_state_sub = rospy.Subscriber('odometry/filtered', Odometry, dhStateCallback)
+  dh_ctrl_eff_sub = rospy.Subscriber('depth_hold/control_effort', Float64, dhControlEffortCallback)
+  dh_toggle_sub = rospy.Subscriber('depth_hold/pid_enable', Bool, dhToggleCallback)
+  depth_sub = rospy.Subscriber('rov/depth_sensor', Float32, change_depth_callback)
+  
+  test_pub = rospy.Publisher('depth_hold/test_depth', Float32, queue_size=1)
+  
+  server = Server(copilotControlParamsConfig, controlCallback)
+  
+  rospy.spin()
+
+if __name__  == "__main__":
+    main()
